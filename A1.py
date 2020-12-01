8196adeee642e57a46e2d3d5067301b6f86c2976
sgzeric <45420211+sgzeric@users.noreply.github.com>
2020-12-02 06:48:26 +0800

    Add files via upload

--- A1.py
+++ A1.py
@@ -18,12 +18,19 @@ data_arr = data[data.columns[1:data.columns.size - 1]].to_numpy()
 
 # Isomap implementation
 def IsoMap(K, array):
-    
-    dist_mat = np.ones([array.shape[0], array.shape[0]])*1000
-    #construct the Adjacency matrix of graph G with k nearest neighbor of each point
+    a=np.zeros((array.shape[0],array.shape[0]))
     for i in range(array.shape[0]):
-        temp = np.argpartition(array[i], K)[:K + 1]
-        dist_mat[i][temp] = array[i][temp]
+        for j in range(array.shape[0]):
+            a[i,j] = np.sqrt(np.sum(np.power((array[i]-array[j]),2)))
+    dist_mat = np.ones([a.shape[0], a.shape[0]])*1000
+    for i in range(a.shape[0]):
+        temp = np.argpartition(a[i], K)[:K + 1]
+        dist_mat[i][temp] = a[i][temp]
+    #dist_mat = []
+    for k in range(dist_mat.shape[0]):
+        for i in range(dist_mat.shape[0]):
+            for j in range(dist_mat.shape[0]):
+                dist_mat[i][j] = min(dist_mat[i][j], dist_mat[i][k]+dist_mat[k][j])
     '''
     dist_mat = np.zeros((array.shape[0], array.shape[0]))
     for i in range(array.shape[0]):
@@ -35,20 +42,23 @@ def IsoMap(K, array):
     for i in range(array.shape[0]):
         inf_mat[i, neighbours[i, :]] = dist_mat[i, neighbours[i, :]]
     '''
-    graph = floyd_warshall(dist_mat)**2
+    #dist_mat = floyd_warshall(dist_mat)**2
 
-    id_mat = np.mat(np.ones(graph.shape[0]))
-    temp1 = graph*id_mat.T*id_mat/graph.shape[0]
-    temp2 = np.array(id_mat.T*id_mat*graph/graph.shape[0])
-    temp = id_mat.T*id_mat*graph*id_mat.T*id_mat/np.power(graph.shape[0], 2)
-    B = -1/2*(graph-temp1-temp2+temp)
+    id_mat = np.mat(np.ones(dist_mat.shape[0]))
+    temp1 = dist_mat*id_mat.T*id_mat/dist_mat.shape[0]
+    temp2 = np.array(id_mat.T*id_mat*dist_mat/dist_mat.shape[0])
+    temp = id_mat.T*id_mat*dist_mat*id_mat.T*id_mat/np.power(dist_mat.shape[0], 2)
+    B = -1/2*(dist_mat-temp1-temp2+temp)
     e_value, e_vector = np.linalg.eigh(B)
     index = np.argsort(-e_value)
     e_vector = e_vector[:,index]
     e_value = e_value[index]
+    #X = np.eye(N=2, M=data_arr.shape[0])@np.sqrt(np.diag(np.abs(e_value)))@np.transpose(e_vector)
+    
     e_vector_k = e_vector[:, [0,1]]
     e_value_k = np.diag(e_value[[0,1]])
     X = np.dot(e_vector_k, np.sqrt(e_value_k))
+    
     return X
 
 # MDS implementation
@@ -98,7 +108,7 @@ def plot_data(array, type, name):
     plt.title(name +' of Animals')
 '''
 if __name__=='__main__':
-    K = 15
+    K = 100
     pca = PCA(n_components=2)
     pca.fit(data_arr)
     pca_arr = pca.transform(data_arr)
@@ -112,8 +122,8 @@ if __name__=='__main__':
     mds_arr = Mds(data_arr).transpose()
     isomap_arr = IsoMap(K, data_arr)
     
-    plot_data(pca_arr, data_type, 'PCA')
-    plot_data(mds_arr, data_type, 'MDS')
+    #plot_data(pca_arr, data_type, 'PCA')
+    #plot_data(mds_arr, data_type, 'MDS')
     #plot_data(sk_mds_arr, data_type, 'MDS_sklearn')
     plot_data(isomap_arr, data_type, 'Isomap K='+ str(K))
     
